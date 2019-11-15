c34bfe851d43624a0040b15687e441a49b3fc1ca
Stephanie <55038687+stephypy@users.noreply.github.com>
2019-11-14 23:18:38 -0700

    Create ProblemA.py

---
+++ ProblemA.py
@@ -0,0 +1,15 @@
+class LRU:
+    def __init__(self):
+        print('ok')
+
+    def get(self, key):
+        print('get val')
+
+    def put(self, key, value):
+        print('insert/replace')
+
+    def size(self):
+       print('size')
+
+    def max_capacity(self):
+        print('max')
